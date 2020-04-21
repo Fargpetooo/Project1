3d6f3f9e5e8222ffaa804f0bf4d15ea8cfbbeb3c
Глебова Александра <55533007+Fargpetooo@users.noreply.github.com>
2020-04-21 13:19:43 +0500

    Add files via upload

---
+++ глава 30 с комментариями/глава 30/CodeFile1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+namespace Petzold.EnlargeButtonWithTimer {
+    public class EnlargeButtonWithTimer : Window {  //объявление класса
+        const double initFontSize = 12;
+        const double maxFontSize = 48;
+        Button btn;
+        [STAThread] 
+            public static void Main() {
+            Application app = new Application();  //инициализация объекта
+            app.Run(new EnlargeButtonWithTimer()); }
+        public EnlargeButtonWithTimer() {
+            Title = "Enlarge Button with Timer";
+            btn = new Button(); //создание кнопки
+            btn.Content = "Expanding Button";
+            btn.FontSize = initFontSize;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;  // выравнивание горизонтальное
+            btn.VerticalAlignment = VerticalAlignment.Center;  // выравнивание по вертикали
+            btn.Click += ButtonOnClick; Content = btn; }
+        void ButtonOnClick(object sender, RoutedEventArgs args) {  //описание кнопки
+            DispatcherTimer tmr = new DispatcherTimer();  
+            tmr.Interval = TimeSpan.FromSeconds(0.1);
+            tmr.Tick += TimerOnTick; tmr.Start(); }
+        void TimerOnTick(object sender, EventArgs args) {  //таймер
+            btn.FontSize += 2;
+            if (btn.FontSize >= maxFontSize) {
+                btn.FontSize = initFontSize;
+                (sender as DispatcherTimer).Stop(); } } } }
